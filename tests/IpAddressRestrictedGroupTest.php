b6568c53a72cb823bad2217875f778b046a407f2
helpfulrobot <chris@silverstripe.com>
2016-01-01 00:57:32 +1300

    Converted to PSR-2

--- tests/IpAddressRestrictedGroupTest.php
+++ tests/IpAddressRestrictedGroupTest.php
@@ -1,24 +1,26 @@
 <?php
-class IpAddressRestrictedGroupTest extends SapphireTest {
-	
-	static $fixture_file = 'IpAddressRestrictedGroupTest.yml';
+class IpAddressRestrictedGroupTest extends SapphireTest
+{
+    
+    public static $fixture_file = 'IpAddressRestrictedGroupTest.yml';
 
-	function testRestrictions() {
-		$unrestricted = $this->objFromFixture('Member', 'unrestricted');
-		$restrictedSingleIp = $this->objFromFixture('Member', 'restricted-singleip');
+    public function testRestrictions()
+    {
+        $unrestricted = $this->objFromFixture('Member', 'unrestricted');
+        $restrictedSingleIp = $this->objFromFixture('Member', 'restricted-singleip');
 
-		$groupUnrestricted = $this->objFromFixture('Group', 'unrestricted');
-		$groupSingleIp = $this->objFromFixture('Group', 'restricted-singleip');
+        $groupUnrestricted = $this->objFromFixture('Group', 'unrestricted');
+        $groupSingleIp = $this->objFromFixture('Group', 'restricted-singleip');
 
-		$this->assertContains($groupUnrestricted->ID, $unrestricted->Groups()->column('ID'));
-		$this->assertContains($groupUnrestricted->ID, $restrictedSingleIp->Groups()->column('ID'));
-		$this->assertNotContains($groupSingleIp->ID, $restrictedSingleIp->Groups()->column('ID'));
+        $this->assertContains($groupUnrestricted->ID, $unrestricted->Groups()->column('ID'));
+        $this->assertContains($groupUnrestricted->ID, $restrictedSingleIp->Groups()->column('ID'));
+        $this->assertNotContains($groupSingleIp->ID, $restrictedSingleIp->Groups()->column('ID'));
 
-		$oldRemote = isset($_SERVER['REMOTE_ADDR']) ? $_SERVER['REMOTE_ADDR'] : null;
-		$_SERVER['REMOTE_ADDR'] = '17.0.0.1';
-		$this->assertContains($groupUnrestricted->ID, $unrestricted->Groups()->column('ID'));
-		$this->assertContains($groupUnrestricted->ID, $restrictedSingleIp->Groups()->column('ID'));
-		$this->assertContains($groupSingleIp->ID, $restrictedSingleIp->Groups()->column('ID'));
-		$_SERVER['REMOTE_ADDR'] = $oldRemote;
-	}
+        $oldRemote = isset($_SERVER['REMOTE_ADDR']) ? $_SERVER['REMOTE_ADDR'] : null;
+        $_SERVER['REMOTE_ADDR'] = '17.0.0.1';
+        $this->assertContains($groupUnrestricted->ID, $unrestricted->Groups()->column('ID'));
+        $this->assertContains($groupUnrestricted->ID, $restrictedSingleIp->Groups()->column('ID'));
+        $this->assertContains($groupSingleIp->ID, $restrictedSingleIp->Groups()->column('ID'));
+        $_SERVER['REMOTE_ADDR'] = $oldRemote;
+    }
 }
